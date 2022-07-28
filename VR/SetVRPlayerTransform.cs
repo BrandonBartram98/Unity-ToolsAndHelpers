e55124463f9b14e487def218271f9c29e8052867
Brandon Bartram <35037470+BrandonBartram98@users.noreply.github.com>
2022-07-28 13:23:07 +0100

    Add files via upload

---
+++ VR/SetVRPlayerTransform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("XR Rig")]
+    [SerializeField] private Transform _xrRig;
+    [SerializeField] private Transform _rigOffset;
+    [SerializeField] private Transform _rigCamera;
+
+    [Header("Transforms")]
+    [SerializeField] private Transform _desiredTransform;
+
+    public void SetPlayerPositionAndRotation()
+    {
+        Vector3 newPos = new Vector3(_desiredTransform.position.x, _xrRig.position.y, _desiredTransform.position.z);
+
+        _xrRig.SetPositionAndRotation(newPos, _desiredTransform.rotation);
+
+        _rigOffset.localRotation = Quaternion.identity;
+        _rigOffset.localRotation = Quaternion.Inverse(Quaternion.Euler(0, _rigCamera.eulerAngles.y, 0)) * _xrRig.rotation;
+    }
+}
