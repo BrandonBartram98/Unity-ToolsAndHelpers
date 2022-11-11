2b12405279eb95e43510f90e9cd06625c950c8c0
Brandon Bartram <35037470+BrandonBartram98@users.noreply.github.com>
2022-11-11 15:05:57 +0000

    Update and rename Helpers.cs to GeneralHelpers.cs

--- GeneralHelpers.cs
+++ GeneralHelpers.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public static class MyHelpers
+public static class GeneralHelpers
 {
     // Camera Reference:
     // Store reference to camera so its only called once
